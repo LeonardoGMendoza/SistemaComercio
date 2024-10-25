9e6d9cbac03c468aee51fe8f28a31ff75c0a30cb
LeonardoGMendoza <desenvolvimento3000@outlook.com>
2024-10-25 12:10:33 -0300

    first commit

---
+++ SistemaComercio/CamadaDados/Conexao.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CamadaDados
+{
+    public class Conexao
+    {
+        public static string Cn = "Data Source = LMCONSULTING\\SQLEXPRESS;Initial Catalog = dbcomercio; Integrated Security=true";
+    }
+}
